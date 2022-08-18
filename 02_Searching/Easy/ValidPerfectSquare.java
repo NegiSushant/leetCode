ed35bdc6d863f0eed270e3ca222d69eb3df8f59d
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-08-18 20:32:41 +0530

    Add files via upload

--- 02_Searching/Easy/ValidPerfectSquare.java
+++ 02_Searching/Easy/ValidPerfectSquare.java
@@ -29,17 +29,17 @@ public class ValidPerfectSquare {
         System.out.println(isPerfectSquare(num));
     }
     public static boolean isPerfectSquare(int num){
-        int start = 1;
-        int end = num;
+        long start = 1;
+        long end = num;
 
         while (start <= end){
-            int mid = start + (end - start) / 2;
+            long mid = start + (end - start) / 2;
             if(mid * mid == num){
                 return true;
-            } else if (mid < num / mid) {
-                start = mid + 1;
-            }else {
+            } else if (mid * mid < num) {
                 end = mid - 1;
+            }else {
+                start = mid + 1;
             }
         }
         return false;
