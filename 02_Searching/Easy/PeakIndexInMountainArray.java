fee4781b910ccb1782d7e485e7eb4fa1f86083af
Sushant Singh Negi <107835408+NegiSushant@users.noreply.github.com>
2022-08-19 20:30:52 +0530

    Add files via upload

---
+++ 02_Searching/Easy/PeakIndexInMountainArray.java
@@ -0,0 +1,68 @@
+/*
+
+https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+10)Ques_252--->>>
+            An array arr a mountain if the following properties hold:
+                  >>arr.length >= 3
+                  >>There exists some i with 0 < i < arr.length - 1 such that:
+                        @) arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+                        @) arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+            Given a mountain array arr, return the index i such that
+            arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+You must solve it in O(log(arr.length)) time complexity.
+
+Example 1:--->>>
+            Input: arr = [0,1,0]
+            Output: 1
+
+Example 2:--->>>
+            Input: arr = [0,2,1,0]
+            Output: 1
+
+Example 3:--->>>
+            Input: arr = [0,10,5,2]
+            Output: 1
+
+
+Constraints:
+               > 3 <= arr.length <= 105
+               > 0 <= arr[i] <= 106
+               > arr is guaranteed to be a mountain array.
+
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PeakIndexInMountainArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter the length of array: ");
+        int n = sc.nextInt();
+
+        System.out.println("enter the numbers in Array: ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Input Array is: "+ Arrays.toString(arr));
+        System.out.println("Peak index in Mountain Array is at the index of : "+peakIndexInMountainArray(arr)+" place in Array.");
+    }
+    public static int peakIndexInMountainArray(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
